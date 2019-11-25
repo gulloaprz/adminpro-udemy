7380a4205f79df55ef96edd36d48e359e693ea89
gulloaprz <guadalupe.ulloa@outlook.com>
2019-11-25 13:13:33 -0600

    Simplifación de los módulos e implementación de rutas hijas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-// Rutas
+// RUTAS
+import { APP_ROUTES } from "./app.routes";
+// MÓDULOS
+import { PagesModule } from "./pages/pages.module";
+// COMPONENTES
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
-import { PagenotfoundComponent } from "./shared/pagenotfound/pagenotfound.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { Graficas1Component } from "./pages/graficas1/graficas1.component";
-import { HeaderComponent } from "./shared/header/header.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
-import { APP_ROUTES } from "./app.routes";
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
+import { RegisterComponent } from "./login/register.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    PagenotfoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    PagesComponent,
-    RegisterComponent
-  ],
-  imports: [BrowserModule, APP_ROUTES],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, APP_ROUTES, PagesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
